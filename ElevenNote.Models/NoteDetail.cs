e70e6ed05f02abf380f7658373224a9c924612ba
bmagee2 <bamagee2@gmail.com>
2020-08-30 22:02:19 -0400

    note controller

---
+++ ElevenNote.Models/NoteDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+
+    // This will let us view all the properties of the note
+
+    // MAKE PUBLIC
+    public class NoteDetail
+    {
+        // PROPERTIES
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Modified")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
